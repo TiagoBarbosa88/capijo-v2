7c4b7f681ffbfb57ff5a0f14a6d887000abb9a0c
Tiago Barbosa <sombr43@gmail.com>
2025-05-22 20:47:02 -0300

    feat: implement artist section with cards and scroll reveal functionality

--- src/app/pages/artistas/artistas.component.ts
+++ src/app/pages/artistas/artistas.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ScrollRevealService } from '../../services/scroll-reveal.service';
 
 @Component({
   selector: 'app-artistas',
   templateUrl: './artistas.component.html',
   styleUrls: ['./artistas.component.scss']
 })
-export class ArtistasComponent {
+export class ArtistasComponent implements OnInit, OnDestroy {
 
+  constructor(private scrollRevealService: ScrollRevealService) { }
+
+  ngOnInit(): void {
+    this.scrollRevealService.init();
+  }
+
+  ngOnDestroy(): void {
+    this.scrollRevealService.cleanup();
+  }
 }
