655acf2eb289201933cf04314841869840ee3a21
Tiago Barbosa <sombr43@gmail.com>
2025-05-22 21:05:39 -0300

    feat: implement studio section with image cards and viewer functionality

--- src/app/pages/estudio/estudio.component.ts
+++ src/app/pages/estudio/estudio.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ImageViewerService } from '../../services/utils/image-viewer.service';
 
 @Component({
   selector: 'app-estudio',
   templateUrl: './estudio.component.html',
   styleUrls: ['./estudio.component.scss']
 })
-export class EstudioComponent {
+export class EstudioComponent implements OnInit {
+  constructor(private imageViewerService: ImageViewerService) { }
 
+  ngOnInit(): void { }
+
+  openImage(imageSrc: string): void {
+    this.imageViewerService.openImage(imageSrc);
+  }
 }
