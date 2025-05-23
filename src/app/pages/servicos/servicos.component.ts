0e7b22db6a1357ac5069e8161088c012ceb21fc8
Tiago Barbosa <sombr43@gmail.com>
2025-05-22 21:30:39 -0300

    feat: update Font Awesome icons to version 5 and enhance services section with event and location details

--- src/app/pages/servicos/servicos.component.ts
+++ src/app/pages/servicos/servicos.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ImageViewerService } from '../../services/utils/image-viewer.service';
 
 @Component({
   selector: 'app-servicos',
   templateUrl: './servicos.component.html',
   styleUrls: ['./servicos.component.scss']
 })
-export class ServicosComponent {
+export class ServicosComponent implements OnInit {
+  constructor(private imageViewerService: ImageViewerService) { }
 
+  ngOnInit(): void { }
+
+  openImage(imageSrc: string): void {
+    this.imageViewerService.openImage(imageSrc);
+  }
 }
