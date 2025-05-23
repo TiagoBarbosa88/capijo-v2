f7973ed3930977b01f64f3ac2e090efcbea57b01
Tiago Barbosa <sombr43@gmail.com>
2025-05-22 21:45:35 -0300

    feat: implement partners section with responsive design and image viewer functionality

--- src/app/pages/parceiros/parceiros.component.ts
+++ src/app/pages/parceiros/parceiros.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ImageViewerService } from '../../services/utils/image-viewer.service';
 
 @Component({
   selector: 'app-parceiros',
   templateUrl: './parceiros.component.html',
   styleUrls: ['./parceiros.component.scss']
 })
-export class ParceirosComponent {
+export class ParceirosComponent implements OnInit {
+  constructor(private imageViewerService: ImageViewerService) { }
 
+  ngOnInit(): void { }
+
+  openImage(imageSrc: string): void {
+    this.imageViewerService.openImage(imageSrc);
+  }
 }
