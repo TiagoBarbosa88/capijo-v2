0207e5fd1d36899bfb2f3270d48cba836a998792
Tiago Barbosa <sombr43@gmail.com>
2026-08-20 21:04:29 -0300

    feat: Update contact phone number across site configuration and HTML files

--- src/app/config/site.config.ts
+++ src/app/config/site.config.ts
@@ -1,8 +1,8 @@
 export const SITE_CONFIG = {
   name: 'Capijo Music Produções',
   baseUrl: 'https://capijo.com',
-  phone: '11-99782-2709',
-  phoneTel: '+5511997822709',
+  phone: '11-9782-2709',
+  phoneTel: '+551197822709',
   email: 'Capijoproducao@gmail.com',
   address: {
     street: 'Pr. Inácia Dias, 64',
@@ -20,7 +20,7 @@ export const SITE_CONFIG = {
     facebook: 'https://www.facebook.com/profile.php?id=61564964385846',
     youtube: 'https://www.youtube.com/@CapijoProdu%C3%A7ao',
     instagram: 'https://www.instagram.com/capijoproducao',
-    whatsapp: 'https://wa.me/5511997822709'
+    whatsapp: 'https://wa.me/551197822709'
   },
   googleSiteVerification: 'seu-código-aqui',
   defaultOgImage: '/assets/img/banner/logo4.png'
