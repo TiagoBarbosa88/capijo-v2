d43711c6608f1c115e8a7aeebc3ff744647c3f69
Tiago Barbosa <sombr43@gmail.com>
2026-09-20 19:24:54 -0300

    feat: Update contact phone number in site configuration and HTML files

--- src/app/config/site.config.ts
+++ src/app/config/site.config.ts
@@ -1,8 +1,8 @@
 export const SITE_CONFIG = {
   name: 'Capijo Music Produções',
   baseUrl: 'https://capijo.com',
-  phone: '11-9782-2709',
-  phoneTel: '+551197822709',
+  phone: '11-97822-7098',
+  phoneTel: '+5511978227098',
   email: 'Capijoproducao@gmail.com',
   address: {
     street: 'Pr. Inácia Dias, 64',
@@ -10,20 +10,20 @@ export const SITE_CONFIG = {
     region: 'SP',
     postalCode: '05203-030',
     country: 'BR',
-    full: 'Pr. Inácia Dias, 64 - Perus, São Paulo - SP, 05203-030'
+    full: 'Pr. Inácia Dias, 64 - Perus, São Paulo - SP, 05203-030',
   },
   geo: {
     latitude: -23.403872,
-    longitude: -46.752944
+    longitude: -46.752944,
   },
   social: {
     facebook: 'https://www.facebook.com/profile.php?id=61564964385846',
     youtube: 'https://www.youtube.com/@CapijoProdu%C3%A7ao',
     instagram: 'https://www.instagram.com/capijoproducao',
-    whatsapp: 'https://wa.me/551197822709'
+    whatsapp: 'https://wa.me/5511978227098',
   },
   googleSiteVerification: 'seu-código-aqui',
-  defaultOgImage: '/assets/img/banner/logo4.png'
+  defaultOgImage: '/assets/img/banner/logo4.png',
 } as const;
 
 export const SITE_ROUTES = [
@@ -33,5 +33,5 @@ export const SITE_ROUTES = [
   'eventos',
   'equipe',
   'parceiros',
-  'contato'
+  'contato',
 ] as const;
